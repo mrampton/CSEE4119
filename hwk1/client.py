5a2f5619fe241caba9af11d746eddda669c798f8
mrampton <mrampton@gmail.com>
2015-10-11 23:32:12 -0400

    Ugh.  Mostly working. but not completely

--- hwk1/client.py
+++ hwk1/client.py
@@ -1,1 +1,94 @@
 #!/usr/bin/env python
+import re
+import sys
+import time
+import socket
+import select
+import threading
+
+BUFFSIZE = 2048
+
+def main(argv):
+	[HOST, PORT] = argv[1:3]
+
+
+
+	if HOST and PORT:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			sock.connect((HOST, int(PORT)))
+		except socket.error:
+			print "Connection refused",
+			exit(-1)
+
+		# import ipdb; ipdb.set_trace()
+		thread = MessagesThread(await_server_response, sock.dup())
+		thread.daemon = True
+		thread.start()
+
+		login_prompt = '^[0-9]?\s?(Username|Password|Command):\s+'
+
+		while True:
+			try:
+				# print "Command: "
+				# r,w,e = select.select([sock], [], [], 1)
+				# data = None
+				# if r:
+				# data = sock.recv(BUFFSIZE) 
+				# print data.strip(),
+				data = sock.recv(BUFFSIZE) 
+				# m = re.match(login_prompt, data)
+				# if m:
+				# 	# print data.strip(),
+				# 	pass
+				# else:
+				# 	print "hmm"
+				# 	print data
+
+				r,w,e = select.select([sock],[],[], 0.01)
+				if r:
+					data = data + sock.recv(BUFFSIZE)
+
+				response = raw_input(data)
+				if response:
+					sock.sendall(response)
+				else: 
+					sock.sendall(" ")
+
+			except KeyboardInterrupt:
+				print "\nClosing Simple Chat Client"
+				break
+
+		sock.close()
+
+
+########################################
+# woker thread; delivers messages to users
+########################################
+
+class MessagesThread(threading.Thread):
+    def __init__(self, target, *args):
+        self._target = target
+        self._args = args
+        threading.Thread.__init__(self)
+ 
+ 	def join(self, timeout=None):
+ 		self.stoprequest.set()
+ 		super(FuncThread, self).join(timeout)
+
+    def run(self):
+        self._target(*self._args)
+
+def await_server_response(sock):
+	
+	while True:
+		time.sleep(1)
+		r,w,e = select.select([sock], [], [], 1)
+		data = None
+		if r:
+			data = sock.recv(BUFFSIZE) 
+			print data.strip()
+
+
+if __name__ == '__main__':
+	main(sys.argv)
--- hwk1/server.py
+++ hwk1/server.py
@@ -1,31 +1,61 @@
 #!/usr/bin/env python
-
+import re
 import sys
+import errno
 import socket
+import select
 import threading
 import time
 import SocketServer
 
-BUFFSIZE        = 1024
-BLOCK_TIME      = 0
-MAX_ATTEMPTS    = 1
+BUFFSIZE        = 2048
+TIME_OUT		= 60 * 30
+BLOCK_TIME      = 10
+MAX_ATTEMPTS    = 3
+
+def main(argv):
+
+	HOST, PORT = "", int(argv[1])
+
+	server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+	server.daemon = True
+	server.import_credentials()
+
+	thread = MessagesThread(await_user_messages)
+	thread.daemon = True
+	thread.start()
+
+	try:
+		server.serve_forever()
+	except IOError, e:
+		if e.errno == 32: pass
+	except (KeyboardInterrupt):
+		print "\nShutting down SimpleChatServer"
+		ThreadedTCPServer.running = False
+
+	server.shutdown()
+	server.server_close()
+	exit(0)
 
 class User():
 
-	def __init__(self, name, ip, logged_in=True):
+	def __init__(self, name, ip, socket, logged_in=True):
 		self.name           = name
-		self.last_login     = time.time()
+		self.last_login     = None
 		self.last_ip        = ip
 		self.logged_in      = logged_in
 		self.blocked_until  = None
+		self.socket			= None
 
-	def log_in(self, ip):
+	def log_in(self, ip, socket):
 		self.last_login     = time.time()
 		self.last_ip        = ip
 		self.logged_in      = True
+		self.socket			= socket
 
 	def log_out(self):
 		self.logged_in      = False
+		self.socket			= None
 
 	def block_ip(self, ip):
 		self.last_ip        = ip
@@ -34,24 +64,13 @@ class User():
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
-	logged_in = {}  
-	lock = threading.Lock()
+	logged_in 		= {}  
+	message_queue 	= {}
+	lock 			= threading.Lock()
 
-	def help(self):
-		msg = [
-			"------------------------------------------------------------------",
-			"whoelse                            List other connected users",
-			"wholast <minutes>                  List users who have logged in",
-			"                                   recently",
-			"broadcast message <message>        Broadcast <message> to all users",
-			"broadcast message <user> <user>    Broadcast <message> to list of users",
-			"... <user> message <message>",
-			"broadcast message <user> <message> Private <message> to a <user>",
-			"logout                             Log out this user",
-			"help                               Display this message",
-			"------------------------------------------------------------------\n"
-			]
-		return "\n".join(msg)
+	########################################
+	# user status and helper methods
+	########################################
 
 	def is_logged_in(self, username):
 
@@ -61,25 +80,23 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 			return False
 
 	def log_out(self, username):
-
-		lock = threading.Lock()
 		with self.lock:
 			self.logged_in[username].log_out()
+		return None
 
 	def log_in(self, username):
 		user = None
-		# lock = threading.Lock()
+		ip = self.current_ip()
 		with self.lock:
 			try:
 				user = self.logged_in[username]
 				if user.logged_in:
 					return False
-				user.log_in(self.current_ip())
-				self.request.sendall("I am in try!\n")
-
-			except:
-				self.request.sendall("I am in except KeyError\n")
-				self.logged_in[username] = User(username, self.current_ip())
+				user.log_in(ip, self.request)
+			except KeyError:
+				user = User(username, ip, self.request)
+				user.log_in(ip, self.request)
+				self.logged_in[username] = user
 
 		return True
 
@@ -98,7 +115,6 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
 		if ((user.blocked_until > time.time() and user.last_ip == self.current_ip())
 				or user.logged_in):
-			print "{}: blocked until {}".format(time.ctime(), time.ctime(user.blocked_until))
 			return True
 
 		return False
@@ -106,13 +122,11 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 	def block_user(self, username):
 		
 		ip = self.current_ip()
-		lock = threading.Lock()
-
 		with self.lock:
 			try:
 				user = self.logged_in[username].block_ip(ip)
 			except KeyError:
-				user = User(username, ip)
+				user = User(username, ip, self.request, False)
 				user.block_ip(ip)
 				self.logged_in[username] = user
 			
@@ -156,54 +170,217 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
 		return user
 
+	########################################
+	# user commands are handled below
+	########################################
+
+	def help(self):
+		msg = [
+			"------------------------------------------------------------------",
+			"whoelse                            List other connected users",
+			"wholast <number>                   Users who logged in during last",
+			"                                   <number> of minutes",
+			"broadcast message <message>        Broadcast <message> to all users",
+			"broadcast user <user> <user>       Broadcast <message> to list of users",
+			"... <user> message <message>",
+			"message <user> <message>           Private <message> to a <user>",
+			"logout                             Log out this user",
+			"help                               Display this message",
+			"------------------------------------------------------------------\n"
+			]
+		return "\n".join(msg)
+
+	def wholast(self, username, time_ago):
+		response = ""
+		for name, user in self.logged_in.items():
+			if time_ago > 0 and user.last_login:
+				if name != username and user.last_login > (time.time() - time_ago * 60):
+					response += "{}\n".format(name)
+		return response
+
+	def whoelse(self, username):
+		response = ""
+		for name, user in self.logged_in.items():
+			if name != username and user.logged_in:
+				response += "{}\n".format(name)
+		return response
+
+	def broadcast_all(self, frm, message):
+		with self.lock:
+			for to, user in self.logged_in.items():
+				if to != frm:
+					msg = "{}: {}".format(frm, message)
+					try:
+						self.message_queue[to].append(msg)
+					except KeyError:
+						self.message_queue[to] = [msg]
+		return None
+
+	def broadcast_users(self, frm, users, message):
+		with self.lock:
+			for to, user in self.logged_in.items():
+				if to != frm and to in users:
+					msg = "{}: {}".format(frm, message)
+					try:
+						self.message_queue[to].append(msg)
+					except KeyError:
+						self.message_queue[to] = [msg]
+		return None
+
+	def message_user(self, frm, to, message):
+		return self.broadcast_users(frm, [to], message)
+
+	########################################
+	# command parser
+	########################################
+
+	def parse_command(self, user, data):
+		response 		= ""		
+
+		# [command, minutes]
+		wholast 		= '^\s*(wholast)\s+([0-9]{1,3})\s*$'
+		# [command, message]
+		broadcast_all 	= '^\s*(broadcast\s+message)\s+(.*)$'
+		# [command, [users], command, message]
+		broadcast_users = '^\s*(broadcast\s+user)\s+(.*)\s(message)\s+(.*)$' 
+		# [command, user, message]
+		message_user	= '^\s*(message)\s+(\S+)(.*)$'
+
+		m = None	
+		if data == "help":
+			return self.help()
+		if data == "whoelse":
+			return self.whoelse(user)
+
+		m = re.match(wholast, data)
+		if m:
+			return self.wholast(user, int(m.group(2)))
+
+		m = re.match(broadcast_all, data)
+		if m:
+			return self.broadcast_all(user, m.group(2))
+
+		m = re.match(broadcast_users, data)
+		if m:
+			return self.broadcast_users(user, m.group(2).split(), m.group(4))
+
+		m = re.match(message_user, data)
+		if m:
+			return self.message_user(user, m.group(2), m.group(3))
+
+		return "Your command is not recognized; type 'help' for help\n"
+
+	########################################
+	# returns messages for the passed in user
+	########################################
+
+	@classmethod
+	def messages_for(cls, user):
+		messages = ""
+		with cls.lock:
+			try:
+				messages = cls.message_queue[user]
+				if messages:
+					cls.message_queue[user] = []
+					return "\n".join(messages) + "\n"
+			except KeyError:
+				pass
+		return None
+
+	########################################
+	# HandlerRequest methods overridden here
+	########################################
+
+	def setup(self):
+		self.request.settimeout(TIME_OUT)
+
 	def handle(self):
 		user = None
 
-		while ThreadedTCPServer.RUNNING:
+		while ThreadedTCPServer.running:
 			prompt = "Command: "
+			thread = None
 			if user:
-				self.request.sendall(prompt)
-				data = self.request.recv(BUFFSIZE)
-				if not data:
-					break
-				cur_thread = threading.current_thread()
-				
-				if data.strip() == "quit":
-					self.log_out(user)
-					user = None
-				if data.strip() == "help":
-					self.request.sendall(self.help())
+				# self.request.sendall(prompt)
+
+				try:
+					data = self.request.recv(BUFFSIZE)
+					if not data: 
+						print "Just logged out", user
+						user = self.log_out(user)
+						break
+
+					data = data.strip()
+
+					if data == "logout":
+						user = self.log_out(user)
+						
+					response = None
+					if data and data != 'logout':
+						response = self.parse_command(user, data)
+
+					if response:
+						self.request.sendall(response + "\n" + prompt)
+					else:
+						self.request.sendall(prompt)
+	
+				except socket.timeout:
+					self.request.sendall("Your connection has timed out\n")
+					user = self.log_out(user)
+
 			else:
-				user = self.authenticate()
+				try:
+					user = self.authenticate()
+					if user: 
+						self.request.sendall(prompt)
+					else:
+						self.request.sendall("\nThere was a problem logging you in\n")
+						break
+				except (IOError, socket.timeout):
+					break
+
+
+########################################
+# woker thread; delivers messages to users
+########################################
+
+class MessagesThread(threading.Thread):
+    def __init__(self, target, *args):
+        self._target = target
+        self._args = args
+        threading.Thread.__init__(self)
+ 
+ 	def join(self, timeout=None):
+ 		self.stoprequest.set()
+ 		super(FuncThread, self).join(timeout)
+
+    def run(self):
+        self._target(*self._args)
+
+def await_user_messages():
+	while ThreadedTCPServer.running:
+
+		time.sleep(0.5)
+		for name, user in ThreadedTCPRequestHandler.logged_in.items():
+			messages = ThreadedTCPRequestHandler.messages_for(name)
+
+			if messages: 
+				messages = '\n' + messages + 'Command: '
+				user.socket.sendall(messages)
+
+########################################
+# TCPServer, inlcude ThreadingMixIn
+########################################
 		
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
-	RUNNING = True 
+	running = True 
 	credentials = {}
 
 	def import_credentials(self):
-
 		for line in open('./user_pass.txt'):
 			user, pw = line.split(" ")
 			self.credentials[user] = pw.strip()
-			print "{} has pw {}".format(user, self.credentials[user])
-
-
-def main(argv):
-
-	HOST, PORT = "localhost", int(argv[1])
-	print HOST, PORT
-
-	server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-	server.import_credentials()
-	try:
-		server.serve_forever()
-	except (KeyboardInterrupt):
-		print "Shutting down SimpleChatServer"
-		ThreadedTCPServer.RUNNING = False
-
-	server.shutdown()
-	server.server_close()
 
 
 if __name__ == '__main__':
